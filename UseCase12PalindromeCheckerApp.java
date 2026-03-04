1c054ce7a7a944347671d2ee71d6dba283847dd8
dhanushhhh-ey <dhanush.200727d@gmail.com>
2026-03-04 21:59:00 +0530

    created UC12

---
+++ UseCase12PalindromeCheckerApp.java
@@ -0,0 +1,39 @@
+import java.util.*;
+
+public class PalindromeCheckerApp{
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Input: ");
+        String input = scanner.nextLine();
+        PalindromeStrategy strategy = new StackStrategy();
+
+        boolean result = strategy.check(input);
+
+        System.out.println("Is Palindrome?: " + result);
+
+        scanner.close();
+    }
+
+}
+interface PalindromeStrategy {
+
+    boolean check(String input);
+}
+class StackStrategy implements PalindromeStrategy {
+    public boolean check(String input) {
+        java.util.Stack<Character> stack = new java.util.Stack<>();
+        for (char c : input.toCharArray()) {
+            stack.push(c);
+        }
+        for (char c : input.toCharArray()) {
+            if (c != stack.pop()) {
+                return false;
+            }
+        }
+
+        return true;
+
+    }
+}
