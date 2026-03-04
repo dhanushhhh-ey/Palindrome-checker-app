9da5dd3e872aa189563759ef96e316fef8dd2c6c
dhanushhhh-ey <dhanush.200727d@gmail.com>
2026-03-04 21:53:57 +0530

    created UC11

---
+++ UseCase11PalindromeCheckerApp.java
@@ -0,0 +1,42 @@
+import java.util.*;
+
+public class PalindromeCheckerApp{
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Input: ");
+        String input = scanner.nextLine();
+
+        PalindromeService service = new PalindromeService();
+
+        if (service.checkPalindrome(input)) {
+            System.out.println("Is Palindrome? : true");
+        } else {
+            System.out.println("Is Palindrome? : false");
+        }
+
+        scanner.close();
+    }
+
+}
+class PalindromeService {
+
+    public boolean checkPalindrome(String input) {
+
+        int start = 0;
+        int end = input.length() - 1;
+
+        while (start < end) {
+
+            if (input.charAt(start) != input.charAt(end)) {
+                return false;
+            }
+
+            start++;
+            end--;
+        }
+
+        return true;
+    }
+}
