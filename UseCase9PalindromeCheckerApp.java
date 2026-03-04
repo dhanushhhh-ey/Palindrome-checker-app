6968ac3fe452ddbaa627803fc0bc03ed73f9fc48
dhanushhhh-ey <dhanush.200727d@gmail.com>
2026-03-04 21:40:16 +0530

    created UC9

---
+++ UseCase9PalindromeCheckerApp.java
@@ -0,0 +1,25 @@
+public class UseCase9PalindromeCheckerApp {
+
+    public static void main(String[] args) {
+
+        String input = "madam";
+
+        boolean isPalindrome = isPalindrome(input, 0, input.length() - 1);
+
+        System.out.println("Input : " + input);
+        System.out.println("Is Palindrome? : " + isPalindrome);
+    }
+
+    private static boolean isPalindrome(String str, int left, int right) {
+
+
+        if (left >= right)
+            return true;
+
+
+        if (str.charAt(left) != str.charAt(right))
+            return false;
+
+        return isPalindrome(str, left + 1, right - 1);
+    }
+}
