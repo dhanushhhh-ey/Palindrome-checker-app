1c8358a078645afde9b66aef4f09503a56cb1181
dhanushhhh-ey <dhanush.200727d@gmail.com>
2026-03-04 22:03:27 +0530

    created UC13

---
+++ UseCase13PalindromeCheckerApp.java
@@ -0,0 +1,57 @@
+import java.util.*;
+
+public class PalindromeCheckerApp{
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Input : ");
+        String input = scanner.nextLine();
+        long start1 = System.nanoTime();
+        boolean result1 = twoPointerCheck(input);
+        long end1 = System.nanoTime();
+        long start2 = System.nanoTime();
+        boolean result2 = stackCheck(input);
+        long end2 = System.nanoTime();
+
+        System.out.println("\nTwo Pointer Result : " + result1);
+        System.out.println("Execution Time : " + (end1 - start1) + " ns");
+
+        System.out.println("\nStack Result : " + result2);
+        System.out.println("Execution Time : " + (end2 - start2) + " ns");
+
+        scanner.close();
+    }
+    private static boolean twoPointerCheck(String input) {
+
+        int left = 0;
+        int right = input.length() - 1;
+
+        while (left < right) {
+            if (input.charAt(left) != input.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+
+        return true;
+    }
+    private static boolean stackCheck(String input) {
+
+        Stack<Character> stack = new Stack<>();
+
+        for (char c : input.toCharArray()) {
+            stack.push(c);
+        }
+
+        for (char c : input.toCharArray()) {
+            if (c != stack.pop()) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+}
